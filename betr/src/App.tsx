d0c70b3cc3796cbc5dfc51a134d24cde1a568449
coltonb3 <clyb333@gmail.com>
2023-01-12 18:26:07 -0600

    updated readme

--- betr/src/App.tsx
+++ betr/src/App.tsx
@@ -2,17 +2,17 @@ import  * as React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Signin from './components/SignIn';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import { deepOrange, deepPurple } from '@mui/material/colors';
+
 
 
 function App() {
   return (
     <div>
     <h1>Test</h1>
-    <Signin/>
+    
     </div>
   );
 }
