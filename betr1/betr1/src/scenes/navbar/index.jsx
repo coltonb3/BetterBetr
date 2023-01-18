7d60d6b0fe0715d0b2f9cb975db791086cf11960
coltonb3 <clyb333@gmail.com>
2023-01-18 10:14:22 -0600

    nav bar

--- betr1/betr1/src/scenes/navbar/index.jsx
+++ betr1/betr1/src/scenes/navbar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormContorl, useTheme, useMediaQuery } from "@mui/material" 
+import { Box, IconButton, InputBase, Typography, Select, MenuItem, FormControl, useTheme, useMediaQuery } from "@mui/material" 
 import { Search, Message, DarkMode, LightMode, Notifications, Help, Menu, Close } from "@mui/icons-material"
 import { useDispatch, useSelector } from 'react-redux'
 import { setMode, setLogout } from 'react-router-dom'
@@ -107,11 +107,59 @@ const NavBar = () => {
           <Close />
         </IconButton>
         </Box>
-      </Box>
+          {/* MENU ITEMS */}
+          <FlexBetween
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="3rem"
+          >
+            <IconButton
+              onClick={() => dispatch(setMode())}
+              sx={{ fontSize: "25px" }}
+            >
+              {theme.palette.mode === "dark" ? (
+                <DarkMode sx={{ fontSize: "25px" }} />
+              ) : (
+                <LightMode sx={{ color: dark, fontSize: "25px" }} />
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: "25px" }} />
+            <Notifications sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} />
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(setLogout())}>
+                  Log Out
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>
+        </Box>
       )}
     </FlexBetween>
-  )
-}
+  );
+};
 
 
 export default NavBar
